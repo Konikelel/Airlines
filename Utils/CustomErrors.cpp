325ec10f4189e8fcd97d769c6a4f1d7ab8fed4f8
Konikelel <wiktoralbinowski@gmail.com>
2024-03-30 22:52:50 +0100

    add more custom errors

--- Utils/CustomErrors.cpp
+++ Utils/CustomErrors.cpp
@@ -7,3 +7,31 @@ const char* NonUniqueIDException::what() const noexcept {
 const char* CannotAllocateMemory::what() const noexcept {
     return "Cannot allocate memory.";
 }
+
+InvalidFlightNr::InvalidFlightNr(const std::string& msg) : message{msg} {
+}
+
+const char* InvalidFlightNr::what() const noexcept {
+    return ("Invalid Flight Number: " + message).c_str();
+}
+
+InvalidPlane::InvalidPlane(const std::string& msg) : message{msg} {
+}
+
+const char* InvalidPlane::what() const noexcept {
+    return ("Invalid Plane: " + message).c_str();
+}
+
+InvalidTime::InvalidTime(const std::string& msg) : message{msg} {
+}
+
+const char* InvalidTime::what() const noexcept {
+    return ("Invalid Time: " + message).c_str();
+}
+
+InvalidCrew::InvalidCrew(const std::string& msg) : message{msg} {
+}
+
+const char* InvalidCrew::what() const noexcept {
+    return ("Invalid Crew: " + message).c_str();
+}
--- Utils/CustomErrors.h
+++ Utils/CustomErrors.h
@@ -1,4 +1,5 @@
 #include <exception>
+#include <string>
 
 #ifndef CUSTOM_ERRORS_H
 #define CUSTOM_ERRORS_H
@@ -11,6 +12,46 @@ class NonUniqueIDException : public std::exception {
 class CannotAllocateMemory : public std::exception {
    public:
     const char* what() const noexcept override;
+};
+
+class InvalidFlightNr : public std::exception {
+   public:
+    InvalidFlightNr(const std::string& msg);
+
+    const char* what() const noexcept override;
+
+   private:
+    std::string message;
+};
+
+class InvalidPlane : public std::exception {
+   public:
+    InvalidPlane(const std::string& msg);
+
+    const char* what() const noexcept override;
+
+   private:
+    std::string message;
+};
+
+class InvalidTime : public std::exception {
+   public:
+    InvalidTime(const std::string& msg);
+
+    const char* what() const noexcept override;
+
+   private:
+    std::string message;
+};
+
+class InvalidCrew : public std::exception {
+   public:
+    InvalidCrew(const std::string& msg);
+
+    const char* what() const noexcept override;
+
+   private:
+    std::string message;
 };
 
 #endif
