3586dce718d032ffb20479f39f21658ac3815ad8
Konikelel <wiktoralbinowski@gmail.com>
2024-04-01 18:13:47 +0200

    add conditional definition StringHandler

--- Utils/StringHandler.h
+++ Utils/StringHandler.h
@@ -1,7 +1,12 @@
 #include <string>
 
+#ifndef STRING_HANDLER_H
+#define STRING_HANDLER_H
+
 std::string toUpper(std::string& sentence);
 
 std::string toLower(std::string& sentence);
 
 std::string toTitle(std::string& sentence);
+
+#endif
