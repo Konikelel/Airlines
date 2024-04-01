5517c6898ffda993b0cd976ec6954b836860e67d
Konikelel <wiktoralbinowski@gmail.com>
2024-04-01 20:01:32 +0200

    add existInVector to VectorHandler

--- Utils/VectorHandler.h
+++ Utils/VectorHandler.h
@@ -6,14 +6,20 @@
 
 // RETURN ITERATOR OF FOUND ELEMENT OF VEC.END(), NEED OVERLOADED ==
 template <typename T>
-typename std::vector<T>::iterator FindInVector(std::vector<T>& vec, const T& target) {
+typename std::vector<T>::iterator findInVector(std::vector<T>& vec, const T& target) {
     return std::find(vec.begin(), vec.end(), target);
+}
+
+// RETURN BOOL IF ELEMENT EXIST IN VECTOR, NEED OVERLOADED ==
+template <typename T>
+bool existInVector(std::vector<T>& vec, const T& target) {
+    return findInVector(vec, target) != vec.end();
 }
 
 // RETURN ITERATOR OF POPPED ELEMENT OF VEC.END(), NEED OVERLOADED ==
 template <typename T>
-typename std::vector<T>::iterator PopFromVector(std::vector<T>& vec, const T& target) {
-    auto targetElement = FindInVector<T>(vec, target);
+typename std::vector<T>::iterator popFromVector(std::vector<T>& vec, const T& target) {
+    auto targetElement = findInVector<T>(vec, target);
 
     if (targetElement == vec.end())
         return vec.end();
