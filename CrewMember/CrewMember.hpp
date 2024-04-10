43cce6c56bc1fa1222ced2825925fc013247fdc4
Konikelel <wiktoralbinowski@gmail.com>
2024-04-10 17:56:38 +0200

    add CrewMember function comments and update Passenger

--- CrewMember/CrewMember.hpp
+++ CrewMember/CrewMember.hpp
@@ -17,11 +17,13 @@ class CrewMember : public Person {
         unsigned int timeBirthday,
         Gender gender);
 
-    void addFlight(Flight* pFlight);
+    // ADD FLIGHT POINTER TO CREW MEMBER, IF CREW MEMBER IS NOT ON THE FLIGHT AND DEPENDING ON WORKING STATUS, INVOKE FUNCTION IN FLIGHT TO ADD CREW MEMBER POINTER TO PASSENGERS OR (STEWARDESSES / PILOTS)
+    void addFlight(Flight* pFlight, bool working);
+    // REMOVE FLIGHT POINTER FROM CREW MEMBER, IF CREW MEMBER IS ON THE FLIGHT, INVOKE FUNCTION IN FLIGHT TO REMOVE CREW MEMBER POINTER
     void removeFlight(Flight* pFlight);
 
-    void removeFlights();
-    void terminate();
+    // REMOVE ALL FLIGHTS FROM CREW MEMBER AND CREW MEMBER POINTERS FROM FLIGHTS
+    bool removeFlights();
 
     bool isBusy(unsigned int timeStart, unsigned int timeEnd);
 
