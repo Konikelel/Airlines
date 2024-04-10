198a5162bad7f9f7286479b8c7b28583a391fbae
Konikelel <wiktoralbinowski@gmail.com>
2024-04-10 18:47:45 +0200

    start implementing company class

--- Company/Company.hpp
+++ Company/Company.hpp
@@ -2,11 +2,24 @@
 #include <vector>
 
 class Company {
+    Company(std::string name);
+
+    void addPlane(Plane* pPlane);
+    bool removePlane(Plane* pPlane);
+
+    void addFlight(Flight* pFlight);
+    bool removeFlight(Flight* pFlight);
+
+    void addStewardess(CrewMember* pStewardess);
+    bool removeStewardess(CrewMember* pStewardess);
+
+    void addPilot(CrewMember* pPilot);
+    bool removePilot(CrewMember* pPilot);
+
    private:
     std::string name;
-    // unsigned int moneyBalance; ???
-
-    // vector CrewMember employee
-    // vector plane planeFleet ???
-    // vector flight flights
+    std::vector<Plane*> planes;
+    std::vector<Plane*> flights;
+    std::vector<CrewMember*> stewardesses;
+    std::vector<CrewMember*> pilots;
 };
