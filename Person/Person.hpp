4669ef73d086758f0e26d561554d0035b024a72f
Konikelel <wiktoralbinowski@gmail.com>
2024-04-10 17:47:27 +0200

    add comments in Person functions

--- Person/Person.hpp
+++ Person/Person.hpp
@@ -24,6 +24,7 @@ class Person {
     unsigned int getTimeBirthday() const;
     Gender getGender() const;
 
+    // SET ID AND REMOVE OLD ONE FROM VECTOR usedIds
     void changeId(const unsigned int id);
     void setNameFirst(const std::string name);
     void setNameSecond(const std::string name);
@@ -31,13 +32,14 @@ class Person {
     void setTimeBirthday(const unsigned int time);
 
    private:
+    // SET ID, CALLED IN CONSTRUCTOR
     void setId(const unsigned int id);
 
     unsigned int id;
     std::string nameFirst;
     std::string nameSecond;
 
-    unsigned int timeBirthday;  // time in miliseconds
+    unsigned int timeBirthday;  // time in miliseconds UTC
     Gender gender;
 };
 
