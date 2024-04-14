079aa69daae34466e3f23faadc783c0a4f46defa
Konikelel <wiktoralbinowski@gmail.com>
2024-04-14 19:04:11 +0200

    cover Passenger with tests

--- Tests/Passenger.tests.cpp
+++ Tests/Passenger.tests.cpp
@@ -11,30 +11,41 @@ TEST(passenger, constructor) {
 }
 
 TEST(passenger, flight) {
-    // Company* pCompany = new (Company){"Test1"};
-    // Flight flight1{pCompany, "RYR123", 1, 2, "Warsaw", "Berlin"};
-    // Flight flight2{pCompany, "RYR123", 3, 4, "Warsaw", "Berlin"};
+    Company* pCompany = new (Company){"Test1"};
+    Flight& flight1 = pCompany->createFlight("RYR123", 1, 2, "Warsaw", "Berlin");
+    Flight& flight2 = pCompany->createFlight("RYR123", 3, 4, "Warsaw", "Berlin");
 
-    // Passenger passenger{1, "Victor", "Alb", 1, MALE};
-    // // Test only if works
-    // //  addFlight
-    // passenger.addFlight(flight1);
-    // EXPECT_TRUE(existVector(passenger.getFlights(), flight1));
-    // EXPECT_TRUE(existVector(flight1.getPassengers(), passenger));
-    // EXPECT_EQ(flight1.getPassengers().size(), 1);
-    // EXPECT_EQ(passenger.getFlights().size(), 1);
-    // // removeFlight
-    // passenger.removeFlight(flight1);
-    // EXPECT_FALSE(existVector(passenger.getFlights(), flight1));
-    // EXPECT_FALSE(existVector(flight1.getPassengers(), passenger));
-    // EXPECT_EQ(flight1.getPassengers().size(), 0);
-    // EXPECT_EQ(passenger.getFlights().size(), 0);
-    // // removeFlights
-    // passenger.addFlight(flight1);
-    // passenger.addFlight(flight2);
-    // passenger.removeFlights();
-    // EXPECT_EQ(flight1.getPassengers().size(), 0);
-    // EXPECT_EQ(passenger.getFlights().size(), 0);
+    Passenger passenger{1, "Victor", "Alb", 1, MALE};
+    // addFlight
+    passenger.addFlight(flight1);
 
-    // delete pCompany, flight1, flight2, passenger;
+    EXPECT_EQ(passenger.getFlights().size(), 1);
+    EXPECT_EQ(flight1.getPassengers().size(), 1);
+
+    EXPECT_TRUE(existVector(passenger.getFlights(), flight1));
+    EXPECT_TRUE(existVector(flight1.getPassengers(), passenger));
+    // removeFlight
+    passenger.addFlight(flight2);
+    EXPECT_TRUE(passenger.removeFlight(flight1));
+
+    EXPECT_EQ(passenger.getFlights().size(), 1);
+    // !EXCEPTION!
+    EXPECT_EQ(flight1.getPassengers().size(), 1);
+    // !EXCEPTION!
+    EXPECT_FALSE(existVector(passenger.getFlights(), flight1));
+    EXPECT_FALSE(existVector(flight1.getPassengers(), passenger));
+    // removeFlights
+    passenger.addFlight(flight1);
+    EXPECT_TRUE(passenger.removeFlights());
+
+    EXPECT_EQ(passenger.getFlights().size(), 0);
+    EXPECT_EQ(flight1.getPassengers().size(), 0);
+
+    EXPECT_FALSE(existVector(passenger.getFlights(), flight1));
+    EXPECT_FALSE(existVector(flight1.getPassengers(), passenger));
+
+    EXPECT_FALSE(existVector(passenger.getFlights(), flight2));
+    EXPECT_FALSE(existVector(flight2.getPassengers(), passenger));
+
+    delete pCompany;
 }
