0362e39650bf0be0c3a679664c9f61e6e02a99b3
Konikelel <wiktoralbinowski@gmail.com>
2024-04-14 22:25:17 +0200

    change overloaded operator== for Flight

--- Utils/VectorHandler.cpp
+++ Utils/VectorHandler.cpp
@@ -4,8 +4,8 @@ bool operator==(const Person& one, const Person& other) {
     return one.getId() == other.getId();
 }
 
-bool operator==(const Flight& one, const Flight& other) {
-    return one.getFlightNr() == other.getFlightNr();  // ADD ID TO FLIGHT
+bool operator==(const Flight& one, const Flight& other) {  // IMPLEMENT ID
+    return one.getFlightNr() == other.getFlightNr() && one.getTimeDeparture() == other.getTimeDeparture() && one.getTimeArrival() == other.getTimeArrival();
 }
 
 bool operator==(const Plane& one, const Plane& other) {
