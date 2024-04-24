dc739146b943cb8c224451737c1e3d1e1ae9c2b8
Konikelel <wiktoralbinowski@gmail.com>
2024-04-24 18:24:59 +0200

    change random number range to fit in unsigned type

--- Utils/IdGenerator.cpp
+++ Utils/IdGenerator.cpp
@@ -6,7 +6,7 @@
 unsigned int generateId() {
     static std::random_device dev;
     static std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> int_dist(1000000000, 9999999999);
+    std::uniform_int_distribution<std::mt19937::result_type> int_dist(100000000, 999999999);
 
     return int_dist(rng);
 }
